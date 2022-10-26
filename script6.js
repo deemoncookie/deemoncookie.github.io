da5c23367fc86a04ee5b940c3fee19c6f6b9c1b4
deemoncookie <114279333+deemoncookie@users.noreply.github.com>
2022-10-26 09:11:15 +0800

    Delete script6.js

--- script6.js
+++
@@ -1,9 +0,0 @@
-let counter = document.querySelector('h1');
-let count = 1;
-setInterval(()=>{
-    counter.innerText = count;
-    count++
-    
-    if(count > 50) location.replace('https://media.discordapp.net/attachments/768786304585695275/1026828746818338937/Untitled.png?width=744&height=498')
-    
-},1000)
